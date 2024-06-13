b975dce89728bfe9c01706c9fec6b9f8ac116d88
Hamburger240329 <165342305+Hamburger240329@users.noreply.github.com>
2024-06-13 15:30:43 +0900

    Add files via upload

--- 07_sidebar.py
+++ 07_sidebar.py
@@ -16,14 +16,15 @@ st.sidebar.write("선택한 작품은:",user_opt)
 
 #메인화면
 st.title("스트림릿의 사이드바")
-folder ='D:/cyj/AI_VSC/data/'
+# folder ='D:/cyj/AI_VSC/data/'
 Image_files=['Vermeer.png', 'Gogh.png','Munch.png','ShinYoonbok.png']
 
 sel_img_index = sel_opt.index(user_opt)
 # 선택한 항목에 맞는 이미지 파일 지정
 img_file = Image_files[sel_img_index]
 # img_local = Image.open('D:/cyj/AI_VSC/data/'+img_file) # PTL 이미지 열기
-img_local = Image.open(folder + img_file) # PTL 이미지 열기
+# img_local = Image.open(folder + img_file) # PTL 이미지 열기
+img_local = Image.open(img_file) # PTL 이미지 열기
 st.image(img_local,caption=user_opt) #이미지 표시
 
 
